addd78d7ea07294bbd2b766c3a4e595a93b35047
L01272 <baldwinkody1@gmail.com>
2025-06-24 23:39:06 -0500

    Finish 06 solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
 
+
+const repeatString = function (phrase, numberOfRepeats) {
+    let finishedPhrase = "";
+    if (numberOfRepeats < 0 ) {
+        return "ERROR";
+    }
+    
+    for (i = 0; i < numberOfRepeats; i++) {
+        console.log('number of repeats: ' + numberOfRepeats);
+        finishedPhrase += phrase;
+        console.log('phrase: ' + phrase);
+        console.log('finished phrase: ' + finishedPhrase);
+    }
+    return finishedPhrase;
 };
 
 // Do not edit below this line
